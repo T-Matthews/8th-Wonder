bea2104be1068e246d315d3974b7c0632cc013e0
Tyler Matthews <100244201+T-Matthews@users.noreply.github.com>
2023-06-02 17:34:54 -0600

    minor tweaks to auth

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request,flash,redirect,url_for
 
 auth = Blueprint('auth',__name__,template_folder='auth_templates',url_prefix='/auth',static_folder='auth_static')
 
@@ -7,5 +7,8 @@ from .authforms import LoginForm
 @auth.route('/login',methods=['GET','POST'])
 def login():
     lform=LoginForm()
+    if request.method =='POST':
+        flash(message="Thanks for logging in",category='message')
+        return redirect(url_for('home'))
 
     return render_template('signin.html',form=lform)

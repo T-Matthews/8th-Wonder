2e74e2b37cba2bc176bf606bcc66345b42327764
Tyler Matthews <100244201+T-Matthews@users.noreply.github.com>
2023-06-05 12:59:51 -0600

    registration, flash messaging

--- app/auth/authforms.py
+++ app/auth/authforms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField(label='Username',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField()
+
+class RegistrationForm(FlaskForm):
+    #what info do i need
+    username=StringField('Username',validators=[DataRequired()])
+    email=StringField('Email', validators=[DataRequired()])
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField()
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,13 +2,43 @@ from flask import Blueprint, render_template,request,flash,redirect,url_for
 
 auth = Blueprint('auth',__name__,template_folder='auth_templates',url_prefix='/auth',static_folder='auth_static')
 
-from .authforms import LoginForm
+from .authforms import LoginForm,RegistrationForm
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
     lform=LoginForm()
     if request.method =='POST':
-        flash(message="Thanks for logging in",category='message')
-        return redirect(url_for('home'))
+        if lform.validate_on_submit():
 
+        #either the form data is proper
+            username= lform.username.data
+            password= lform.password.data
+            print('formdata: ',username,password)
+            flash(message=f"Thanks for logging in, {username}",category='primary')
+            #
+            return redirect(url_for('home'))
+        #or the username is incorrect - we dont want to sign the user in
+        else:
+            return redirect(url_for('auth.login'))
     return render_template('signin.html',form=lform)
+
+
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if request.method =="POST":
+        if form.validate_on_submit():
+            #check db
+            # access form data
+            print(form.data)
+            flash('Welcome! Thank you for registering.','info')
+            return redirect(url_for('home'))
+        else:
+            flash('Sorry, your passwords do not match. Please try again.','danger')
+            return redirect(url_for('auth.register'))
+
+
+    #implied else, activated upon GET request
+    return render_template('register.html',form=form)
+    
